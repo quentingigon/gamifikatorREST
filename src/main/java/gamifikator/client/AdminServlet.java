570c6df33c62c216ec338a8b6d4b766179266de0
Yohann Meyer <yohann.meyer@gmail.com>
2018-11-15 20:18:00 +0100

    no use

--- src/main/java/gamifikator/client/AdminServlet.java
+++ src/main/java/gamifikator/client/AdminServlet.java
@@ -7,13 +7,19 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Stateless
 @WebServlet(name = "AdminServlet", urlPatterns = "/admin")
 public class AdminServlet extends GenericServlet {
 
 	public String test = "5";
-	@Override
+    List<String> list = Arrays.asList("1", "2", "3");
+
+
+    @Override
 	public void init(ServletConfig config) throws ServletException {
 		super.init(config);
 	}
@@ -21,6 +27,7 @@ public class AdminServlet extends GenericServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.setAttribute("test",test); //Setting UsernameLabel to mes_add_pageTitle
+		req.setAttribute("list",list);
 		req.getRequestDispatcher(ADMIN_JSP).forward(req, resp);  //forwarded to welcome.jsp
 	}
 

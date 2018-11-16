478881bd4523b9cd5600b3a9e6be0022af502058
Yohann Meyer <yohann.meyer@gmail.com>
2018-11-16 10:56:39 +0100

    docker working.

---
+++ docker/images/mysql/src/script_db.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE IF NOT EXISTS gamifikator;
+USE gamifikator;
+
+CREATE TABLE USER
+(
+  email VARCHAR(50) NOT NULL,
+  username VARCHAR(50) NOT NULL,
+  password VARCHAR(100) NOT NULL,
+  isAdmin BOOL NOT NULL,
+  isSuspended BOOL NOT NULL,
+  isPasswordValid BOOL NOT NULL,
+  PRIMARY KEY (email)
+);
+
+CREATE TABLE APPLICATION
+(
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(50) NOT NULL,
+  owner_email VARCHAR(50) NOT NULL,
+  description VARCHAR(400),
+	apisecret VARCHAR(100) UNIQUE,
+  apikey VARCHAR(100) UNIQUE,
+  isDeployed BOOL NOT NULL,
+  PRIMARY KEY (id),
+	FOREIGN KEY (owner_email) REFERENCES USER(email)
+);

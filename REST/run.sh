b1d86845f9a9598eb6b00f22ae6df3077522e26c
Yohann Meyer <yohann.meyer@gmail.com>
2019-01-24 12:42:04 +0100

    dockerisation mamene

--- REST/run.sh
+++ REST/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 cp target/swagger-spring-1.0.0.jar docker/images/springboot/swagger-spring_jar.jar
+cp db/script_db.sql docker/images/mysql/src/script_db.sql 
 cd docker
 docker-compose up --build
+cd ..

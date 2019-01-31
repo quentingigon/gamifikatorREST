417d9ba613b12f686cf52f45e47850f957035642
Gigon Quentin <gigon.quentin@gmail.com>
2019-01-31 14:10:15 +0100

    put correct script for docker

--- REST/docker/images/mysql/src/script_db.sql
+++ REST/docker/images/mysql/src/script_db.sql
@@ -62,3 +62,8 @@ CREATE TABLE application_entity
   is_deployed BOOL NOT NULL,
   PRIMARY KEY (id)
 );
+
+
+INSERT INTO application_entity
+(name, owner, description, api_secret, api_token, is_deployed)
+VALUES ("App1", "moi", "description", "secret", "token", false);

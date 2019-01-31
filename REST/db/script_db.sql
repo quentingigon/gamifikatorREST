ae4a34112eca61b009a2ae0adfacaea34d172d57
Gigon Quentin <gigon.quentin@gmail.com>
2019-01-31 14:01:08 +0100

    small modif to db script for docker

--- REST/db/script_db.sql
+++ REST/db/script_db.sql
@@ -62,3 +62,10 @@ CREATE TABLE application_entity
   is_deployed BOOL NOT NULL,
   PRIMARY KEY (id)
 );
+
+
+USE gamifikator;
+
+INSERT INTO application_entity
+(name, owner, description, api_secret, api_token, is_deployed)
+VALUES ("App1", "moi", "description", "secret", "token", false);
